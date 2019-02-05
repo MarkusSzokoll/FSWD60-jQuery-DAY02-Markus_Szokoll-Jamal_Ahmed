759b2bc2d90fcf2ea4caa8c1c667a1e0100ca468
JamalAhmed2019 <jamalusb@gmx.at>
2019-02-05 12:47:40 +0100

    intermediate

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-$(document).ready(function(){
-     $("img").on('click', function(){
-     	if(this.id==0){
-		     document.getElementById('msg').innerHTML = "You clicked gift";
- 		     }
-	     else if(this.id==1){
-			document.getElementById('msg').innerHTML = "You clicked santa";
-	     }
-	     else{
-	     	document.getElementById('msg').innerHTML = "You clicked the bell";
-	     }
-	     
+$(document).ready(function() {
+    $("img").on('click', function() {
+        if (this.id == "chris") {
+            document.getElementById('msg').innerHTML = "You clicked gift";
+        } else if (this.id == "claus") {
+            document.getElementById('msg').innerHTML = "You clicked santa";
+        } else {
+            document.getElementById('msg').innerHTML = "You clicked the bell";
+        }
 
 
-	      
 
     });
 });
---
+++ scriptcourt.js
@@ -0,0 +1,16 @@
+  $(document).ready(function(i){
+    
+    $('a').attr('onclick', function () {
+       var list=document.createElement("li")
+	var node=document.createTextNode("i")
+	document.getElementById("list").appendChild("list")
+}
+        
+    });
+
+
+
+  
+
+
+   
