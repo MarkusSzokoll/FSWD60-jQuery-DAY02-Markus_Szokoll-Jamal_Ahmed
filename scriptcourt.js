24e970a6ad030951f183cdf9008028570946e5d2
JamalAhmed2019 <jamalusb@gmx.at>
2019-02-05 14:39:37 +0100

    shopping cart

--- scriptcourt.js
+++ scriptcourt.js
@@ -1,10 +1,23 @@
   $(document).ready(function(i){
-    
-    $('a').attr('onclick', function () {
-       var list=document.createElement("li")
-	var node=document.createTextNode("i")
-	document.getElementById("list").appendChild("list")
+    var cart=[];
+     function additem (name,price) {
+     	var item = {name: name, price: price};
+     	for(var i in cart){
+     		if (cart[i].name==name){
+     		document.getElementById("item").innerHTML= name ;
+     			return;
+     		}
+     	}
+     	cart.push(item)
+       
 }
+
+$(".add-to-cart").on('click',function(event){
+	additem("A gift from santa-claus")
+
+   
+    
+})
         
     });
 
