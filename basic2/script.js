647cab8e7d54ae62a45ca270836f85830e62bee6
Markus Szokoll <markus.szokoll@gmx.at>
2019-02-05 10:28:49 +0100

    basic2

---
+++ basic2/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+     	var innerStr = "";
+ 		for(i=0;i<3;i++){
+ 			for(j=0;j<3;j++){
+ 				innerStr += "<img ='"+i+"' src='santa-claus.png'>";
+ 			}
+ 			innerStr += "<br>";
+ 		}
+ 		document.getElementById("msg").innerHTML = innerStr;
+
+ 	$("img").on('click', function(){
+ 		this.remove();
+    });
+});
+
+
+
